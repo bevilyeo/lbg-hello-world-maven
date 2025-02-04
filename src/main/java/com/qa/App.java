abeb27cb4525ca9ce9cf82552cbe0b78a5470f2b
Bevil <bevil78@gmail.com>
2025-02-04 14:17:54 +0000

    names amended

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -9,8 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(sayHello());
-        System.out.println(sayHelloToSomeone("Alice"));
-        System.out.println(sayHelloToSomeone("Bob"));
+        System.out.println(sayHelloToSomeone("Bev"));
+        System.out.println(sayHelloToSomeone("Pekwor"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
